7b7d7be5419184212f80d0f95b91ffecc9f757bf
Phạm Dương <pduongpdu99@gmail.com>
2025-12-14 23:04:52 +0700

    refactor: formatted

--- src/common/interfaces/const.ts
+++ src/common/interfaces/const.ts
@@ -1,29 +1,26 @@
 export interface DB {
-    HOST: string;
-    PORT: number;
-    USERNAME: string;
-    PASSWORD: string;
-    DATABASE: string;
+  HOST: string
+  PORT: number
+  USERNAME: string
+  PASSWORD: string
+  DATABASE: string
 }
 
 export interface IConfiguration {
-    DEFAULT_VALUE: {
-        PAGINATION: {
-            LIMIT: string;
-            PAGE: string;
-            ORDER?: string;
-            Q?: string;
-        };
-    },
-    DATABASE: {
-        MYSQL?: DB
-        MONGO?: Record<string, string>
-    },
-    REDIS: {
-
-    },
-    RATE_LIMITING: {
-        LIMIT: number,
-        TTL: number // miliseconds
+  DEFAULT_VALUE: {
+    PAGINATION: {
+      LIMIT: string
+      PAGE: string
+      ORDER?: string
+      Q?: string
     }
+  }
+  DATABASE: {
+    MYSQL?: DB
+    MONGO?: Record<string, string>
+  }
+  RATE_LIMITING: {
+    LIMIT: number
+    TTL: number // miliseconds
+  }
 }
