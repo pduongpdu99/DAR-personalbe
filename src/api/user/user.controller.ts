ca849ff4d9bd2b70e836e7214c8c526e46b91927
Phạm Dương <pduongpdu99@gmail.com>
2025-12-15 13:39:01 +0700

    update user controller

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,18 @@
+import { UserCreate } from '@dto/user-create'
+import { UserUpdate } from '@dto/user-update'
+import { User } from '@entities/user.entity'
+import { CRUDController } from '@impl/controller'
 import { Controller } from '@nestjs/common'
+import { UserService } from './user.service'
+import { ENDPOINT } from '@common/const'
 
-@Controller('user')
-export class UserController {}
+@Controller(ENDPOINT.USER)
+export class UserController extends CRUDController<
+  UserCreate,
+  UserUpdate,
+  User
+> {
+  constructor(service: UserService) {
+    super(service)
+  }
+}
