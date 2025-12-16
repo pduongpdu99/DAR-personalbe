eb8c8c3d9189f3e0c760109c59791e24eabcd1f4
Phạm Dương <pduongpdu99@gmail.com>
2025-12-16 12:34:34 +0700

    add typeorm, interceptor, config

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -2,11 +2,13 @@ import { UserCreate } from '@dto/user-create'
 import { UserUpdate } from '@dto/user-update'
 import { User } from '@entities/user.entity'
 import { CRUDController } from '@impl/controller'
-import { Controller } from '@nestjs/common'
+import { Controller, UseInterceptors } from '@nestjs/common'
 import { UserService } from './user.service'
 import { ENDPOINT } from '@common/const'
+import { FormatResponseInterceptor } from '@common/middlewares/format-response'
 
 @Controller(ENDPOINT.USER)
+@UseInterceptors(FormatResponseInterceptor)
 export class UserController extends CRUDController<
   UserCreate,
   UserUpdate,
--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '@entities/user.entity'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [ConfigService, UserService],
 })
 export class UserModule {}
