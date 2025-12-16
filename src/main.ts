e3fcaa7e9e4e74a39b11c1b86b6b3220f4b8b90e
Phạm Dương <pduongpdu99@gmail.com>
2025-12-16 12:33:36 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import helmet from 'helmet'
+// import { KafkaOptions, Transport } from '@nestjs/microservices'
+import { config } from 'dotenv'
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+config({
+  path: '.env.development.local',
+})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  // await NestFactory.createMicroservice<KafkaOptions>(AppModule, {
+  //   transport: Transport.KAFKA,
+  //   options: {
+  //     client: {
+  //       brokers: [process.env.AMQP_URL ?? 'localhost:9092'],
+  //     },
+  //   },
+  // })
+
   app.enableCors()
   app.use(helmet())
+
+  app.setGlobalPrefix('apis')
 
   await app.listen(process.env.PORT ?? 3000)
 }
