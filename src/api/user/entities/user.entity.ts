2b4f506e1c5cf4ea661b1ce489898ef80c029e50
Phạm Dương <pduongpdu99@gmail.com>
2025-12-16 22:28:26 +0700

    update field user

--- src/api/user/entities/user.entity.ts
+++ src/api/user/entities/user.entity.ts
@@ -1,7 +1,17 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
-import { IsNumber } from 'class-validator'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { IsNumber, IsOptional } from 'class-validator'
 import { CONFIGURATION } from '@common/const'
-import { DecorEnum, DecorString } from '@common/decorator-combined'
+import {
+  DecorBoolean,
+  DecorEnum,
+  DecorString,
+} from '@common/decorator-combined'
 import { PhoneCode } from '@common/enums/define-field'
 
 @Entity(CONFIGURATION.ORM.ALIAS.USER)
@@ -9,6 +19,28 @@ export class User {
   @PrimaryGeneratedColumn()
   @IsNumber()
   id: number
+
+  @CreateDateColumn({ name: 'created_date' })
+  createdDate: Date | string | number
+
+  @UpdateDateColumn({ name: 'updated_date' })
+  updatedDate: Date | string | number
+
+  @Column({ name: 'created_by', type: 'int', nullable: true })
+  @IsOptional()
+  createdBy?: number
+
+  @Column({ name: 'updated_by', type: 'int', nullable: true })
+  @IsOptional()
+  updatedBy?: number
+
+  @DecorBoolean({
+    name: 'is_active',
+    defaultValue: true,
+    isOptional: true,
+  })
+  @IsOptional()
+  isActive?: boolean
 
   @DecorString({ name: 'firstname', length: 20, isOptional: true })
   firstName: string
