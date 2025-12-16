111d43765dd25d0d228afa97ff05cbe16c6913e2
Phạm Dương <pduongpdu99@gmail.com>
2025-12-16 12:32:27 +0700

    update db condfig

--- src/common/modules/db.ts
+++ src/common/modules/db.ts
@@ -1,4 +1,5 @@
 import { CONFIGURATION } from '@common/const'
+import { User } from '@entities/user.entity'
 import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
@@ -6,26 +7,31 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'mysql',
-          host: config.get(
-            'MYSQL_HOSTNAME',
-            CONFIGURATION.DATABASE.MYSQL?.HOST,
-          ),
-          port: config.get('MYSQL_PORT', CONFIGURATION.DATABASE.MYSQL?.PORT),
-          username: config.get(
-            'MYSQL_USERNAME',
-            CONFIGURATION.DATABASE.MYSQL?.USERNAME,
-          ),
-          password: config.get(
-            'MYSQL_PASSWORD',
-            CONFIGURATION.DATABASE.MYSQL?.PASSWORD,
-          ),
-        }
-      },
       inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        autoLoadEntities: true,
+        host: config.get('MYSQL_HOSTNAME', CONFIGURATION.DATABASE.MYSQL?.HOST),
+        port: config.get('MYSQL_PORT')
+          ? Number(config.get('MYSQL_PORT'))
+          : CONFIGURATION.DATABASE.MYSQL?.PORT,
+        username: config.get(
+          'MYSQL_USERNAME',
+          CONFIGURATION.DATABASE.MYSQL?.USERNAME,
+        ),
+        password: config.get(
+          'MYSQL_PASSWORD',
+          CONFIGURATION.DATABASE.MYSQL?.PASSWORD,
+        ),
+        database: config.get(
+          'MYSQL_DATABASE',
+          CONFIGURATION.DATABASE.MYSQL?.DATABASE,
+        ),
+        entities: [User],
+        // logging: true,
+        synchronize: true,
+      }),
     }),
   ],
 })
-export class DatabaseModule {}
+export class DBModule {}
