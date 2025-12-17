1b1dba349da4a037af8b9e11d6a276d5a7a1b344
Phạm Dương <pduongpdu99@gmail.com>
2025-12-17 09:40:32 +0700

    setup sse

--- src/api/chat/chat.controller.ts
+++ src/api/chat/chat.controller.ts
@@ -2,10 +2,11 @@ import { ChatCreate } from '@dto/chat-create'
 import { ChatUpdate } from '@dto/chat-update'
 import { Chat } from '@entities/chat.entity'
 import { CRUDController } from '@impl/controller'
-import { Controller, UseInterceptors } from '@nestjs/common'
+import { Controller, Sse, UseInterceptors } from '@nestjs/common'
 import { ChatService } from './chat.service'
 import { ENDPOINT } from '@common/const'
 import { FormatResponseInterceptor } from '@common/middlewares/format-response'
+import { interval, map } from 'rxjs'
 
 @Controller(ENDPOINT.CHAT)
 @UseInterceptors(FormatResponseInterceptor)
@@ -16,5 +17,16 @@ export class ChatController extends CRUDController<
 > {
   constructor(service: ChatService) {
     super(service)
+  }
+
+  @Sse('bot-response')
+  retrieveBotRepsonse() {
+    const milisecs = 1000
+    return interval(milisecs).pipe(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      map((_) => {
+        return { hello: 'world' }
+      }),
+    )
   }
 }
